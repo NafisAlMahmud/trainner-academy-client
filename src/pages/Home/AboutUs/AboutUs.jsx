ab6f3596dfddb839ec1e85937893240b1107c84c
Project_3 <anik2115@gmail.com>
2023-06-12 16:39:06 +0600

    about us updated

--- src/pages/Home/AboutUs/AboutUs.jsx
+++ src/pages/Home/AboutUs/AboutUs.jsx
@@ -14,14 +14,18 @@ const AboutUs = () => {
         <div className="md:w-3/4">
           <h2 className="font-bold text-3xl text-left ">About Us</h2>
           <p className="pt-8">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
-            necessitatibus! Lorem ipsum, dolor sit amet consectetur adipisicing
-            elit. Labore, accusantium quia, quae eveniet deleniti alias culpa
-            consequatur distinctio sit, libero eius voluptatum quidem odio in!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
-            necessitatibus! Lorem ipsum, dolor sit amet consectetur adipisicing
-            elit. Labore, accusantium quia, quae eveniet deleniti alias culpa
-            consequatur distinctio sit, libero eius voluptatum quidem odio in!
+            Welcome to our football training website! We are dedicated to
+            providing a comprehensive platform for football enthusiasts of all
+            ages and skill levels. Whether you're a beginner looking to learn
+            the basics or an experienced player aiming to enhance your
+            technique, we have the resources and expertise to help you achieve
+            your goals. At our website, we understand the importance of proper
+            training and the impact it can have on your performance. That's why
+            we have assembled a team of highly qualified coaches and trainers
+            who are passionate about the game and committed to helping you
+            improve. Our training programs are carefully designed to cover all
+            aspects of football, including technical skills, tactical knowledge,
+            physical conditioning, and mental resilience.
           </p>
         </div>
       </div>
